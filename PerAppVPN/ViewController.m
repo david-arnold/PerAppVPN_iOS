e11e97bfe998ef4cc21152f46f2840481f7b9cbd
David Arnold <darnold@blackberry.com>
2017-12-14 04:49:58 -0500

    Per app vpn sample - this project simply initiates a call to an internal server, if per-app-vpn is applied via MDM control then the connection should be initialised

--- PerAppVPN/ViewController.m
+++ PerAppVPN/ViewController.m
@@ -25,5 +25,29 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void) makeNetworkRequest:(id)sender {
+    NSOperationQueue *operationQueue = [NSOperationQueue mainQueue];
+    
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:self delegateQueue:operationQueue];
+    
+    //perhaps generate a guid and append to the URL to prevent caching ?
+    
+    [[[NSURLSessionConfiguration defaultSessionConfiguration] URLCache] removeAllCachedResponses];
+    
+    NSUUID *uuid = [[NSUUID alloc]init];
+    
+    NSString *urlString = [NSString stringWithFormat:@"http://attts-sql2008.attlab.sw.rim.net/Test/userinfo.aspx?randomGuid=%@",[uuid UUIDString]];
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:urlString]];
+    
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request
+                                            completionHandler:
+                                  ^(NSData *data, NSURLResponse *response, NSError *error) {
+                                      NSLog(@"Asynchronous NSURLSessionDataTask completionHandler");
+                                  }];
+    
+    [task resume];
+}
+
 
 @end
